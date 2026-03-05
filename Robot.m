c6fb691c3b4160857e385904376800852050b052
Brighton Lee <leeba@rose-hulman.edu>
2026-03-05 17:58:53 -0500

    Completed inverse kinematics challenge possibly

--- Robot.m
+++ Robot.m
@@ -71,7 +71,7 @@ classdef Robot < handle
             for i = 1:length(transition_points)-1
                 ll(end+1) = transition_points(i+1)-transition_points(i);
             end
-            ll
+            ll;
         end
 
         % Function to get theta values
@@ -134,8 +134,8 @@ classdef Robot < handle
             end
             
             % Print results
-            K
-            phi
+            K;
+            phi;
         end
 
         % Take in all robot dependent parameters (lls, phi, kappa) and
@@ -176,6 +176,200 @@ classdef Robot < handle
             exp_tran = [exp_tran_1, exp_tran_2;
                        0, 0, 0, 1];
             T = exp_rot*exp_tran;
+        end
+
+
+        % Inverse kinematics; calculates the control inputs for the
+        % concentric tube robot given a current pose and a goal end
+        % effector position
+        function [rho,theta] = invkinematics(self, q_curr, goal_pos)
+            % First we get the rho and theta avlues from q_curr
+            rho_curr = get_rho_values(self, q_curr);
+            theta_curr = get_theta(self, q_curr);
+
+            % Next, we use rho to get the link lengths
+            self.lls = get_links(self, rho_curr);
+
+            % Now we calculate the phi and kappa values
+            [self.phi,self.kappa] = calculate_phi_and_kappa(self, theta_curr);
+
+            c = [];
+            for i = 1:length(self.lls)
+                c(end+1) = self.kappa(i);
+                c(end+1) = self.phi(i);
+                c(end+1) = self.lls(i);
+            end
+
+            % Finally we calculate the base to end effector transform
+            T_curr = calculate_transform(self, self.lls, self.phi, self.kappa);
+
+            % Get current end effector position
+            p_curr = T_curr(1:3, 4);
+
+            % Calculate (p-p*)
+            err = goal_pos - p_curr;
+
+            % Calculate J_p, the Jacobian mapping task space to arc parameters    
+            J = jacob0(self, c);
+                
+            J_omega = J(1:3, :);
+            J_v_space = J(4:6, :);
+                
+            p_skew = [ 0,         -p_curr(3),  p_curr(2);
+                       p_curr(3),  0,         -p_curr(1);
+                      -p_curr(2),  p_curr(1),  0        ];
+                      
+            J_p = J_v_space - p_skew * J_omega;
+               
+            % Modification of equation 12 from paper
+            % Calculate change in arc parameters required to move to goal position
+            J_pinv = pinv(J_p); 
+            delta_c = J_pinv * err;
+                
+            % Calculate goal arc parameters
+            c_goal = c + delta_c;
+
+            % Solve robot-dependent inverse kinematics using an
+            % optimization problem (Newton's Method with a central
+            % difference gradient)
+            rho = rho_curr;
+            theta = theta_curr;
+            
+            max_iter = 100;
+            tol = 1e-8;
+            
+            for i = 1:max_iter               
+                % Robot dependent forward kinematics with guess
+                [phi_guess, k_guess] = calculate_phi_and_kappa(self, theta);
+                ll_guess = get_links(self, rho);
+
+                c_guess = [];
+                for i = 1:length(ll_guess)
+                    c_guess(end+1) = k_guess(i);
+                    c_guess(end+1) = phi_guess(i);
+                    c_guess(end+1) = ll_guess(i);
+                end
+                
+                % Residual
+                R = c_guess - c_goal;
+                
+                % Gradient
+                J = func_gradient(rho,theta);
+                
+                % Optimization
+                dq = J\-R;
+                
+                % Update guess
+                rho = rho + dq(1:length(rho));
+                theta = theta + dq(length(rho)+1,end);
+                
+                % End condition (error converges)
+                if norm(dq) < tol
+                    break
+                end
+            end
+        end
+        
+        % Gradient function for robot-dependent inverse kinematics
+        % optimization problem
+        function J = func_gradient(rho,theta)
+
+            h = 1e-6;
+            J = [];
+            
+            [phi, k] = calculate_phi_and_kappa(self, theta);
+            ll = get_links(self, rho);
+
+            for i = 1:length(rho)
+                rho_ph = rho;
+                rho_mh = rho;
+                rho_ph(i) = rho_ph(i) + h;
+                rho_mh(i) = rho_mh(i) - h;
+                ll_ph = get_links(self, rho_ph);
+                ll_mh = get_links(self, rho_mh);
+                
+                c_ph = [];
+                for i = 1:length(ll_ph)
+                    c_ph(end+1) = k(i);
+                    c_ph(end+1) = phi(i);
+                    c_ph(end+1) = ll_ph(i);
+                end
+
+                c_mh = [];
+                for i = 1:length(ll_mh)
+                    c_mh(end+1) = k(i);
+                    c_mh(end+1) = phi(i);
+                    c_mh(end+1) = ll_mh(i);
+                end
+
+                J(:, i) = (c_ph-c_mh)/(2*h);
+            end
+
+            for i = 1:length(theta)
+                theta_ph = theta;
+                theta_mh = theta;
+                theta_ph(i) = theta_ph(i) + h;
+                theta_mh(i) = theta_mh(i) - h;
+                [phi_ph, k_ph] = calculate_phi_and_kappa(self, theta_ph);
+                [phi_mh, k_mh] = calculate_phi_and_kappa(self, theta_mh);
+
+                c_ph = [];
+                for i = 1:length(ll_ph)
+                    c_ph(end+1) = k_ph(i);
+                    c_ph(end+1) = phi_ph(i);
+                    c_ph(end+1) = ll(i);
+                end
+
+                c_mh = [];
+                for i = 1:length(ll_mh)
+                    c_mh(end+1) = k_mh(i);
+                    c_mh(end+1) = phi_mh(i);
+                    c_mh(end+1) = ll(i);
+                end
+
+                J(:, i+length(rho)) = (c_ph-c_mh)/(2*h);
+            end
+        end
+
+        
+        % Robot independent continuum Jacobian solution recycled from HW 4
+        function J = jacob0(self, c)
+            J = [];
+            ll = [];
+            dphi = [];
+            K = [];
+            for i = 1:length(c)/3
+                K(end+1) = c(3*(i-1)+1);
+                dphi(end+1) = c(3*(i-1)+2);
+                ll(end+1) = c(3*(i-1)+3);
+            end
+            
+            for i = 1:length(c)/3
+                k = K(i);
+                phi = dphi(i);
+                l = ll(i);
+                Jnew = [-l*sin(phi), 0, -k*sin(phi);
+                          l*cos(phi), 0, k*cos(phi);
+                         0, 1, 0;
+                         cos(phi)*(cos(k*l)-1)/(k^2), 0, 0;
+                         sin(phi)*(cos(k*l)-1)/(k^2), 0, 0;
+                         -(sin(k*l)-k*l)/(k^2), 0, 1];
+                J(:,end+1:end+3) = Jnew;
+            end
+            
+            for i = 2:length(c)/3
+               T = calculate_transform(self, ll(1:i-1), dphi(1:i-1), K(1:i-1));
+               R = T(1:3,1:3);
+               p = T(1:3,4);
+               p_square = [0, -p(3), p(2);
+                           p(3), 0, -p(1);
+                           -p(2), p(1), 0];
+               AdT = [R, zeros(3); p_square * R, R];
+                
+               for j = 1:3
+                  J(:,3*(i-1)+j) = AdT*J(:,3*(i-1)+j);
+               end
+            end
         end
     end
 end
---
+++ go_to_position.m
@@ -0,0 +1,41 @@
+%% Set up
+clear; clc;
+
+% startPose = Pose(0, 0, 0, 0, 0, 0);
+% drive_bot = Drive(startPose);
+
+%% INPUT
+% Initial joint values (for 2 tube case)
+% q_initial = [0, 0, 0, 0; 
+%              20, 50, 45, -45; 
+%              30, 35, -35, 20];
+
+% Initial joint values (for 3 tube case)
+q_initial = [0, 0, 0, 0, 0, 0;
+             20, 50, 70, 45, -45, 45; 
+             30, 35, 50, -35, 20, -10];
+
+% Goal position
+goal_pos = [0, 0, 0]';
+
+%% CALCULATIONS
+tube1 = Tube(3.046*10^-3, 3.3*10^-3, 1/17, 90*10^-3, 50*10^-3, 1935*10^6);
+tube2 = Tube(2.386*10^-3, 2.64*10^-3, 1/22, 170*10^-3, 50*10^-3, 1935*10^6);
+tube3 = Tube(1.726*10^-3, 1.98*10^-3, 1/29, 250*10^-3, 50*10^-3, 1935*10^6);
+
+% Uncomment one of the below lines, depending if you are testing a 2-tube or 3-tube case
+tubes = [tube1, tube2];
+% tubes = [tube1, tube2, tube3];
+
+robot = Robot(tubes);
+
+[rho, theta] = robot.invkinematics(q_initial, goal_pos)
+
+%% MOVE
+
+% Move robot for 2 tube case
+% drive_bot.travel_for(rho(1), rho(2), theta(1), theta(2))
+
+% Move robot for 3 tube case
+% drive_bot.travel_for(rho(1), rho(2), rho(3), theta(1), theta(2), theta(3))
+
